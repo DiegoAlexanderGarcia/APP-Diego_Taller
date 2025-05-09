3b5a28880957902da4e1c710862b873d149db83b
DiegoGarcia <dgarciarodriguez370@gmail.com>
2025-05-09 11:28:26 -0500

    Add ApiError class and .gitkeep for validations directory

---
+++ src/main/java/com/electro/electro_app/domain/DTOs/ApiError.java
@@ -0,0 +1,13 @@
+package com.electro.electro_app.domain.DTOs;
+
+import java.io.Serializable;
+
+public class ApiError implements Serializable{
+
+}
+
+// import java.io.Serializable;
+// import java.util.List;
+
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.ResponseStatus;
